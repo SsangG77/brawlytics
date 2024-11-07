ff68530961972c94e5738831d942953d8011a564
sangjin <sangjincha719@gmail.com>
2024-11-08 01:27:40 +0900

    검색바에 검색기록 나오게 하기

---
+++ ViewModel/CalculateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalculateViewModel.swift
+//  Brawlytics
+//
+//  Created by 차상진 on 11/7/24.
+//
+
+import Foundation
---
+++ Views/CalculateView/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Brawlytics
+//
+//  Created by 차상진 on 11/7/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
