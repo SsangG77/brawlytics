265b08c11bac2fc22599f3f2764ae7319fe24b9a
sangjin <sangjincha719@gmail.com>
2024-11-17 19:52:59 +0900

    기본 브롤러 정보들 배열 정의

---
+++ test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  Brawlytics
+//
+//  Created by 차상진 on 11/16/24.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
