c94586b0e0978e1d21fc5838cb4e503d7d691937
sangjin <sangjincha719@gmail.com>
2024-11-19 23:26:01 +0900

    하이퍼차지뷰 생성

--- ViewModel/BrawlerViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BrawlerViewModel.swift
-//  Brawlytics
-//
-//  Created by 차상진 on 11/6/24.
-//
-
-import Foundation
---
+++ Views/HyperchargeView/HyperchargeView.swift
@@ -0,0 +1,18 @@
+//
+//  HyperchargeView.swift
+//  Brawlytics
+//
+//  Created by 차상진 on 11/19/24.
+//
+
+import SwiftUI
+
+struct HyperchargeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HyperchargeView()
+}
