ea958f0d62c02608c59df061618bac58cdb9e13d
sangjin <sangjincha719@gmail.com>
2024-11-23 03:38:13 +0900

    로딩 스케레톤 뷰 입력

---
+++ Module/BlinkingAnimationModifier.swift
@@ -0,0 +1,18 @@
+//
+//  BlinkingAnimationModifier.swift
+//  Brawlytics
+//
+//  Created by 차상진 on 11/23/24.
+//
+
+import SwiftUI
+
+struct BlinkingAnimationModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlinkingAnimationModifier()
+}
